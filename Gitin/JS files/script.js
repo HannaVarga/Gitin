076492aeb0696b54138e2dd02490b66a601e8120
Jay Goos <jaygoos@Jays-MacBook-Pro.local>
2023-04-03 16:03:26 +0200

    added target attribute for opening in new tab

--- Gitin/JS files/script.js
+++ Gitin/JS files/script.js
@@ -73,7 +73,7 @@ searchButton.addEventListener("click",async function(e){
       return` <div class="card-flex-container">
                 <div class="profile-card-container">
                   <div class="image-container"> 
-                    <a href="${item.github_url}"><img src="./Images/github icon.png" id="github-icon"></a>
+                    <a href="${item.github_url}" target="_blank"><img src="./Images/github icon.png" id="github-icon"></a>
                     <img src="${item.avatar}" id="profile-picture">
                     <img src="./Images/linkedin icon.png" id="linkedin-icon">
                   </div>

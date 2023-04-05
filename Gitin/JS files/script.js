aadd6452b7d148297d07d429fbb167be5f953743
Jay Goos <jaygoos@Jays-MacBook-Pro.local>
2023-04-05 10:03:14 +0200

    moved the api call to the else statement, so the API call won't crash anymore

--- Gitin/JS files/script.js
+++ Gitin/JS files/script.js
@@ -16,85 +16,78 @@ searchButton.addEventListener("click",async function(e){
   const experience = document.getElementById("experience-entry").value;
   const repos= document.getElementById("repos-entry").value;
 
-  // //     //**Target the empty element within our HTML **/
-  // const redAlert = document.querySelector(".alert");
-  // //     //**Pass in a condition that checks if form is empty**/
-  // if (location == "") {
-  //     redAlert.innerText = "Please Fill Out Form";
-  //     redAlert.style.color = "red"
-  // } else if (language == "") {
-  //     redAlert.innerText = "Please Fill Out Form";
-  //     redAlert.style.color = "red"
-  // } else if (experience == "") {
-  //     redAlert.innerText = "Please Fill Out Form";
-  //     redAlert.style.color = "red"
-  // } else if (repos == "") {
-  //     redAlert.innerText = "Please Fill Out Form";
-  //     redAlert.style.color = "red"
-  // } else {
-  //     //         //**Display the user a thank you message for filling form **/
-  //     redAlert.innerText = "Thank You For Submitting!"
-  //     redAlert.style.color = "#AAFF00";
-  // };
-
-  console.log(location,language,experience,repos);
-  const userGroupArray = await getUserGroup(language,location,repos);
-  console.log(userGroupArray);
-  const userArray = userGroupArray.json.items.slice(0,6);
-  console.log(userArray);
-  const userLoginArray = [];
-  for (let i = 0; i < userArray.length; i++) {
-    const element = userArray[i].login;
-    userLoginArray.push(element);
-    
-  }
-  console.log(userLoginArray);
-  
-  let userDetailsArray = [];
-  for (let i = 0; i < userLoginArray.length; i++) {
-    let userName = userLoginArray[i];
-    let userDetails = await getUserDetails(userName);
-    const userObject = {
-                        name: userDetails.json.name,
-                        experience: userDetails.json.created_at,
-                        followers: userDetails.json.followers,
-                        repositories: userDetails.json.public_repos,
-                         location: userDetails.json.location,
-                         company: userDetails.json.company, 
-                         avatar: userDetails.json.avatar_url,
-                         github_url: userDetails.json.html_url,
-                         repo_url: userDetails.json.html_url + '/?tab=repositories',
-                         followers_url: userDetails.json.html_url + '/?tab=followers'
-     }
-     userDetailsArray.push(userObject);
-
-  }
-  //console.log(userDetailsArray);
-  const displaySearch = (array) => {
-    const searchResult = array.map((item)=>{
-      return` <div class="card-flex-container">
-                <div class="profile-card-container">
-                  <div class="image-container"> 
-                    <a href="${item.github_url}" target="_blank"><img src="./Images/github icon.png" id="github-icon"></a>
-                    <img src="${item.avatar}" id="profile-picture">
-                    <img src="./Images/linkedin icon.png" id="linkedin-icon">
-                  </div>
-                  <div class="text-flex-container">
-                    <h4>${item.name}</h4>
-                    <p>${language} Developer</p>
-                     <h4>${item.location}</h4>
-                   </div>
-                   <div class="btn-container">
-                     <button id="repositories-btn" onClick="window.open('${item.repo_url}');">Reopsitories:${item.repositories}</button>
-                     <button id="followers-btn" onClick="window.open('${item.followers_url}');">Followers: ${item.followers}</button>
-                   </div>
-                 </div>
-               </div> `;
+    //     //**Target the empty element within our HTML **/
+    const redAlert = document.querySelector(".alert");
+    //     //**Pass in a condition that checks if form is empty**/
+    if (location == "") {
+        redAlert.innerText = "Please Fill Out Form";
+        redAlert.style.color = "red"
+    } else if (language == "") {
+        redAlert.innerText = "Please Fill Out Form";
+        redAlert.style.color = "red"
+    } else if (experience == "") {
+        redAlert.innerText = "Please Fill Out Form";
+        redAlert.style.color = "red"
+    } else if (repos == "") {
+        redAlert.innerText = "Please Fill Out Form";
+        redAlert.style.color = "red"
+    } else {
+        console.log(location,language,experience,repos);
+        const userGroupArray = await getUserGroup(language,location,repos);
+        console.log(userGroupArray);
+        const userArray = userGroupArray.json.items.slice(0,6);
+        console.log(userArray);
+        const userLoginArray = [];
+        for (let i = 0; i < userArray.length; i++) {
+          const element = userArray[i].login;
+          userLoginArray.push(element);
+          
         }
-    );
-    insertMainPage(mainDefault,searchResult).join("");
-};  
-  
-  displaySearch(userDetailsArray);
-  
+        console.log(userLoginArray);
+        
+        let userDetailsArray = [];
+        for (let i = 0; i < userLoginArray.length; i++) {
+          let userName = userLoginArray[i];
+          let userDetails = await getUserDetails(userName);
+          const userObject = {
+                              name: userDetails.json.name,
+                              experience: userDetails.json.created_at,
+                              followers: userDetails.json.followers,
+                              repositories: userDetails.json.public_repos,
+                              location: userDetails.json.location,
+                              company: userDetails.json.company, 
+                              avatar: userDetails.json.avatar_url,
+                              github_url: userDetails.json.html_url,
+                              repo_url: userDetails.json.html_url + '/?tab=repositories',
+                              followers_url: userDetails.json.html_url + '/?tab=followers'
+          }
+          userDetailsArray.push(userObject);
+      
+        }
+        //console.log(userDetailsArray);
+        const displaySearch = (array) => {
+          const searchResult = array.map((item)=>{
+            return` <div class="card-flex-container">
+                      <div class="profile-card-container">
+                        <div class="image-container"> 
+                          <a href="${item.github_url}" target="_blank"><img src="./Images/github icon.png" id="github-icon"></a>
+                          <img src="${item.avatar}" id="profile-picture">
+                          <img src="./Images/linkedin icon.png" id="linkedin-icon">
+                        </div>
+                        <div class="text-flex-container">
+                          <h4>${item.name}</h4>
+                          <p>${language} Developer</p>
+                          <h4>${item.location}</h4>
+                        </div>
+                        <div class="btn-container">
+                          <button id="repositories-btn" onClick="window.open('${item.repo_url}');">Reopsitories:${item.repositories}</button>
+                          <button id="followers-btn" onClick="window.open('${item.followers_url}');">Followers: ${item.followers}</button>
+                        </div>
+                      </div>
+                    </div> `;
+            });
+      insertMainPage(mainDefault,searchResult).join("");
+      };  
+      displaySearch(userDetailsArray);
+    };
 });
